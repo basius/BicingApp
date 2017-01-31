13dc14ee5d773400c9e51ff88d115c65fa346b42
basius <jorabada@gmail.com>
2017-01-31 17:39:11 +0100

    Creem MapFragment

--- app/src/main/java/com/example/basius/bicingapp/MainActivityFragment.java
+++ app/src/main/java/com/example/basius/bicingapp/MainActivityFragment.java
@@ -17,6 +17,8 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
     }
 }
